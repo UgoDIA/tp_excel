2c908d81ccb14de33256796d9a305b4ff7e0f689
UgoDIA <112081131+UgoDIA@users.noreply.github.com>
2025-04-10 07:14:10 +0200

    Update tests.py

--- src/apps/authentification/tests.py
+++ src/apps/authentification/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class AuthTest(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(
+            username='testuser', password='testpass')
+
+    def test_home_page_authenticated(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get('/tp_excel/tp1/')
+        self.assertEqual(response.status_code, 200)
