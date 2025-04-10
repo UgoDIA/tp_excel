df2ebc20da638677a8afe82768cfb3d2a0a7ad4e
UgoDIA <112081131+UgoDIA@users.noreply.github.com>
2025-04-10 11:56:41 +0200

    Update tests.py

--- src/apps/authentification/tests.py
+++ src/apps/authentification/tests.py
@@ -3,10 +3,10 @@ from django.contrib.auth import get_user_model
 
 
 class AuthTest(TestCase):
-    def setUp(self):
-        User = get_user_model()
-        self.user = User.objects.create_user(
-            username='testuser', password='testpass')
+    # def setUp(self):
+    #     User = get_user_model()
+    #     self.user = User.objects.create_user(
+    #         username='testuser', password='testpass')
 
     def test_home_page_authenticated(self):
         self.client.login(username='testuser', password='testpass')
